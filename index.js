085956e0c6d686d5ad4b30d89619462018098649
Paul <54538759+eoSFwae@users.noreply.github.com.>
2025-07-17 22:30:19 -0400

    cors removed and added scripts + dist

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const cors = require('cors');
 
 
 morgan.token('body',(req,res)=>{return JSON.stringify(req.body)})
 
-app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
